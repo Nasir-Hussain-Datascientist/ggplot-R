263835ed6a054eef366ab2efe8a4d63e3f26b085
Nasir Hussain <nasir.swat.hussain@gmail.com>
2024-10-07 00:02:09 +0500

    more into data

--- summaries.R
+++ summaries.R
@@ -16,3 +16,29 @@ install.packages("datasauRus")
 library(datasauRus)
 
 ggplot(datasaurus_dozen,aes(x=x,y=y,colour=dataset)) + geom_point() + theme_void() + theme(legend.position = "none") + facet_wrap(~dataset,ncol=3)
+install.packages("SimDesign")
+library(SimDesign)
+
+
+actual_temp <- c(68.3,70,72.4,71,67,70)
+predicted_temp <- c(67.9, 69, 71.5, 70, 67, 69)
+
+
+bias(actual_temp, predicted_temp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
