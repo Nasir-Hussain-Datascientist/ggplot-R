92c483859ce4ff73d2329ce56fa7fa64ec577c3d
Nasir Hussain <nasir.swat.hussain@gmail.com>
2024-10-09 21:48:48 +0500

    astehtics

--- penguins data cleaning.R
+++ penguins data cleaning.R
@@ -9,4 +9,12 @@ View(penguins)
 ggplot(penguins) + geom_point(mapping = aes(x=flipper_length_mm,y=body_mass_g))
 ggplot(penguins,aes(x=bill_length_mm,y=bill_depth_mm)) + geom_point()
 
-?geom
+#-------------------------------------------------------------------
+
+library(tidyverse)
+library(ggplot2)
+library(palmerpenguins)
+ggplot(data=penguins) +
+  geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,alpha=species,),color="purple") 
+ggplot(data=penguins) +
+  geom_jitter(mapping=aes(x=flipper_length_mm,y=body_mass_g,linetype=species),color="purple",)
